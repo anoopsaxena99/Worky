3cb207eff15f99d81d476293dd3a33bb23f283c4
anoops <anoopsaxena99@gmail.com>
2022-03-25 15:28:36 +0530

    connected customer.html with sign up page and store the information of the worker to the Person datbase

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from flask import Flask
 from flaskext.mysql import MySQL
 from pymysql import NULL
 import yaml
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,redirect
 from datetime import datetime
+from flask_login import login_user, login_required, logout_user, current_user
 
 app=Flask(__name__)
 
@@ -19,19 +20,16 @@ app.config["MYSQL_DATABASE_PASSWORD"] = db['mysql_password']
 app.config["MYSQL_DATABASE_DB"] = db['mysql_db']
 mysql=MySQL(app)
 
-<<<<<<< HEAD
+
+# class customer_work(db.model1):
+#   sno = db.Column(db.Integer, primery_key = True)
+#   work_title = db.Column(db.String(300), nullable = True)
+#   desc = db.Column(db.String(500), nullable = True)
+#   wotype = db.Column(db.String(50), nullable = True)
+#   Location = db.Column(db.String(50), nullable = True)
+#   date_created = db.Column(db.Datetime, default = datetime.utcnow)
+
 #routes on the website
-=======
-class customer_work(db.model1):
-  sno = db.Column(db.Integer, primery_key = True)
-  work_title = db.Column(db.String(300), nullable = True)
-  desc = db.Column(db.String(500), nullable = True)
-  wotype = db.Column(db.String(50), nullable = True)
-  Location = db.Column(db.String(50), nullable = True)
-  date_created = db.Column(db.Datetime, default = datetime.utcnow)
-
-
->>>>>>> 5ecbaebca4bdf71a0574892ca8e3f240e09bfaf1
 @app.route('/')
 def home():
   return render_template('home.html')
@@ -46,12 +44,17 @@ def support():
 
 @app.route('/login')
 def login():
+  if request.method == 'POST':
+    userdetails=request.form
   return render_template('login.html')
-
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('auth.login'))
 @app.route('/customer')
 def customer():
   return render_template('customer.html')
-
+    
 @app.route('/signup',methods=['GET','POST'])
 def signup():
   if request.method == 'POST':
@@ -68,6 +71,8 @@ def signup():
     dob = request.form.get('dob')
     Adress=request.form.get('adress')
 
+
+
         #skills details
     NA=request.form.get('one')
     Labour=request.form.get('two')
@@ -83,19 +88,24 @@ def signup():
     #Cursor of the database
 
     cur = mysql.get_db().cursor()
-    cur.execute("SELECT * from users where email='%s'"%email1)
-    rows = cur.fetchall()
-    if(rows!=NULL) : 
-      x=0
-      for row in rows:
-        print(row[0])
-        x=x+1
-      print("{0} no of entries already there".format(x))   
-      return("{0} no of entries already there".format(x))  
-    cur.execute("INSERT INTO users(name,email) VALUES(%s,%s)",(name ,email))
+
+    cur.execute("INSERT INTO PERSON(MobileNo,Password,AdharNumber,Name,DOB) VALUES(%s,%s,%s,%s,%s)",(Num,password1,Adhar_Id,name,dob))
     mysql.get_db().commit()
     cur.close()
-    return('SUCCESS')    
+    return render_template('customer.html')
+    # cur.execute("SELECT * from users where email='%s'"%email1)
+    # rows = cur.fetchall()
+    # if(rows!=NULL) : 
+    #   x=0
+    #   for row in rows:
+    #     print(row[0])
+    #     x=x+1
+    #   print("{0} no of entries already there".format(x))   
+    #   return("{0} no of entries already there".format(x))  
+    # cur.execute("INSERT INTO users(name,email) VALUES(%s,%s)",(name ,email))
+    # mysql.get_db().commit()
+    # cur.close()
+    # return('SUCCESS')    
   return render_template("signup.html")  
 
 if __name__ == '__main__' :
