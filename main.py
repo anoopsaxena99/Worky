6d5507830deff009f38e0e173a14f05c0753db25
deepak <ramkumardeepak774@gmail.com>
2022-03-25 10:43:54 +0530

    nothing

--- main.py
+++ main.py
@@ -19,19 +19,16 @@ app.config["MYSQL_DATABASE_PASSWORD"] = db['mysql_password']
 app.config["MYSQL_DATABASE_DB"] = db['mysql_db']
 mysql=MySQL(app)
 
-<<<<<<< HEAD
 #routes on the website
-=======
-class customer_work(db.model1):
-  sno = db.Column(db.Integer, primery_key = True)
-  work_title = db.Column(db.String(300), nullable = True)
-  desc = db.Column(db.String(500), nullable = True)
-  wotype = db.Column(db.String(50), nullable = True)
-  Location = db.Column(db.String(50), nullable = True)
-  date_created = db.Column(db.Datetime, default = datetime.utcnow)
+#class customer_work(db.model1):
+ # sno = db.Column(db.Integer, primery_key = True)
+  #work_title = db.Column(db.String(300), nullable = True)
+  #desc = db.Column(db.String(500), nullable = True)
+  #wotype = db.Column(db.String(50), nullable = True)
+  #Location = db.Column(db.String(50), nullable = True)
+  #date_created = db.Column(db.Datetime, default = datetime.utcnow)
 
 
->>>>>>> 5ecbaebca4bdf71a0574892ca8e3f240e09bfaf1
 @app.route('/')
 def home():
   return render_template('home.html')
